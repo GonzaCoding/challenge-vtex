3c664562a390621df1a136cd6f861063dbb0c522
giglesias <giglesias@appsolut.us>
2020-10-25 02:18:20 -0300

    Detalle de Pelicula

---
+++ src/components/MovieDetail.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react'
+
+export const MovieDetail = ({ movieId }) => {
+    //console.log(movieId);
+    const [movieDetail, setMovieDetail] = useState({});
+    const [movieCast, setMovieCast] = useState([]);
+    const [loadingMovieDetail, setLoadingMovieDetail] = useState(true);
+    const [loadingMovieCast, setLoadingMovieCast] = useState(true);
+
+    useEffect(() => {
+        getMovieDetail(movieId);
+        getMovieCast(movieId);
+    }, []);
+
+    const getMovieDetail = async (movieId) => {
+        
+        const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=cba2729e2b09eabe7bcd684a1788211d&language=en-US`;
+
+        const fetchData = await fetch(url);
+
+        const jsonData = await fetchData.json();
+                
+        setMovieDetail({
+            title: jsonData.title,
+            img: "https://image.tmdb.org/t/p/w342" + jsonData.poster_path,
+            genres: jsonData.genres,
+            year: jsonData.release_date.substring(0, 4)
+        });
+        setLoadingMovieDetail(false);
+    }
+
+    const getMovieCast = async (movieId) => {
+        
+        const urlCast = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=cba2729e2b09eabe7bcd684a1788211d`;
+
+        const fetchData = await fetch(urlCast);
+
+        const jsonData = await fetchData.json();
+        setMovieCast(jsonData.cast);
+        setLoadingMovieCast(false);
+    }
+
+    const {title, img, genres, year} = movieDetail;
+
+    return (
+        <div>
+            { !loadingMovieDetail &&
+                <>
+                    <h1>{title}</h1>
+                    <img src={img} />
+                    <p>Release year: {year}</p>
+                    <p>Genres: 
+                    <div className="genres-list">
+                    {
+                        genres.map(genre => (
+                            <span className="movie-genre" key={genre.id}>{genre.name}</span>
+                        ))
+                    }
+                    </div>
+                    
+                    </p>
+                    <p>Cast:</p>
+                    <ul>
+                    { 
+                        movieCast.map(cast => {
+                            //console.log(cast.name);
+                            return(
+                                <li className="movie-cast" key={cast.cast_id}>{cast.name}</li>
+                            )
+                            
+                        })
+                    } 
+                    </ul>
+                      
+                    
+                    
+                </>
+            }
+            
+        </div>
+    )
+}
