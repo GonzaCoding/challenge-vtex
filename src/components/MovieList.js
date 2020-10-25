c5cbb4c7885842d0cab7ff8a581054481daf7745
giglesias <giglesias@appsolut.us>
2020-10-25 00:08:53 -0300

    Maquetacion basica componente MovieList

---
+++ src/components/MovieList.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const movies=[
+    {
+        id: 1,
+        title: "Movie 1",
+        img: "https://m.media-amazon.com/images/M/MV5BN2NkZjA0OWUtZDgyMy00MjIxLWJhZTEtYjdmYzVjZTQ3YWRiL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_UY1200_CR85,0,630,1200_AL_.jpg",
+        year: 2002
+    },
+    {
+        id: 2,
+        title: "Movie 2",
+        img: "https://m.media-amazon.com/images/M/MV5BN2NkZjA0OWUtZDgyMy00MjIxLWJhZTEtYjdmYzVjZTQ3YWRiL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_UY1200_CR85,0,630,1200_AL_.jpg",
+        year: 2006
+    },
+    {
+        id: 3,
+        title: "Movie 3",
+        img: "https://m.media-amazon.com/images/M/MV5BN2NkZjA0OWUtZDgyMy00MjIxLWJhZTEtYjdmYzVjZTQ3YWRiL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_UY1200_CR85,0,630,1200_AL_.jpg",
+        year: 2008
+    },
+    {
+        id: 4,
+        title: "Movie 4",
+        img: "https://m.media-amazon.com/images/M/MV5BN2NkZjA0OWUtZDgyMy00MjIxLWJhZTEtYjdmYzVjZTQ3YWRiL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_UY1200_CR85,0,630,1200_AL_.jpg",
+        year: 1999
+    },
+
+]
+
+export const MovieList = () => {
+    return (
+        <section className="movie-list">
+            {
+                movies.map(movie =>(
+                    <article className="movie-card" key={movie.id}>
+                        <img className="movie-img" src={movie.img} />
+                        <h3 className="movie-title" >{movie.title}</h3>
+                        <p className="movie-year">{movie.year}</p>
+                        <button className="btn-details">See details</button>
+                    </article>
+                ))
+            }
+        </section>
+    )
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { MovieList } from '../components/MovieList'
 
 export const HomePage = () => {
     return (
         <div>
             <h1>Home Page</h1>
+            <MovieList />
         </div>
     )
 }
